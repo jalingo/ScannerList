74e44c713f6b4a8559676ddb43c01b1e34fae2e2
jalingo <james@lingotechsolutions.com>
2017-04-10 21:32:17 -0700

    implemented QR / barcode scanner...need to test on real hardware!

---
+++ ScannerList/PreScanner.swift
@@ -0,0 +1,38 @@
+//
+//  PreScanner.swift
+//  ScannerList
+//
+//  Created by Jimmy Lingo on 4/10/17.
+//  Copyright © 2017 Promethatech. All rights reserved.
+//
+
+import UIKit
+
+class PreScanner: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Navigation
+    
+    @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
